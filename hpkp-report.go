bd237fa93a54f45ef2bfbc2a7f9ee7c4a737f52b
Jean Paul Galea <jp@galea.se>
2016-09-11 21:01:55 +0200

    Initial commit.
    - biggest weekened hack ever.
    
    - lot of refactoring and polishing is needed.

---
+++ hpkp-report.go
@@ -0,0 +1,311 @@
+package main
+
+import (
+	"crypto/x509"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+	"net/http"
+	"net"
+	"regexp"
+	"strings"
+	"time"
+	"log"
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+	"fmt"
+)
+
+var db *sql.DB
+
+type report struct {
+	DateTime                  time.Time `json:"date-time"`
+	EffectiveExpirationDate   time.Time `json:"effective-expiration-date"`
+	Hostname                  string    `json:"hostname"`
+	NotedHostname             string    `json:"noted-hostname"`
+	Port                      uint16    `json:"port"`
+	IncludeSubdomains         bool      `json:"include-subdomains"`
+	ServedCertificateChain    []string  `json:"served-certificate-chain"`
+	ValidatedCertificateChain []string  `json:"validated-certificate-chain"`
+	KnownPins                 []string  `json:"known-pins"`
+}
+
+type KnownPin struct {
+	Name string
+	Value string
+}
+
+type Certificate struct {
+	PEM string
+	Pin string
+	Position int
+}
+
+func main() {
+	var err error
+	db, err = sql.Open("mysql", "root:abcd@/hpkp")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	http.HandleFunc("/", ReceiveReport)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ReceiveReport(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(400)
+		return
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	var r report
+	err := decoder.Decode(&r)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	ip := RequestIP(req)
+	user_agent := req.Header.Get("User-Agent")
+
+	kpins, err := KnownPins(r.KnownPins)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	certs_s, err := CertificateChain(r.ServedCertificateChain)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+	certs_v, err := CertificateChain(r.ValidatedCertificateChain)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	err = violation(r, ip, user_agent, kpins, certs_s, certs_v)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func RequestIP(req *http.Request) string {
+	s := req.Header.Get("X-Forwarded-For")
+	if s == "" {
+		s = req.RemoteAddr
+	}
+
+	// drop port if necessary
+	host, _, err := net.SplitHostPort(s)
+	if err == nil {
+		s = host
+	}
+
+	ip := net.ParseIP(s)
+	if ip != nil {
+		return ip.String()
+	}
+	return ""
+}
+
+func KnownPins(pins []string) ([]KnownPin, error) {
+	re := regexp.MustCompile("\\Apin-sha256=(\"[^\"]+\"|'[^']+')\\z")
+
+	p := make([]KnownPin, 0, len(pins))
+
+	for _, pin := range pins {
+		r := re.FindStringSubmatch(pin)
+		if r == nil {
+			return p, errors.New("")
+		}
+
+		s := strings.Trim(r[1], "\"'")
+
+		data, err := base64.StdEncoding.DecodeString(s)
+		if err != nil {
+			return p, errors.New("")
+		}
+		if len(data) != 32 {
+			return p, errors.New("")
+		}
+
+		p = append(p, KnownPin{Name: "pin-sha256", Value: s})
+	}
+
+	return p, nil
+}
+
+func CertificateChain(PEMCerts []string) ([]Certificate, error) {
+	c := make([]Certificate, 0, len(PEMCerts))
+
+	for i, PEMCert := range PEMCerts {
+		block, _ := pem.Decode([]byte(PEMCert))
+		if block == nil {
+			return c, errors.New("")
+		}
+		x509cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return c, errors.New("")
+		}
+		p, err := PKPSHA256Hash(x509cert)
+		if err != nil {
+			return c, errors.New("")
+		}
+
+		c = append(c, Certificate{PEM: PEMCert, Pin: p, Position: i})
+	}
+
+	return c, nil
+}
+
+func PKPSHA256Hash(cert *x509.Certificate) (string, error) {
+	var s string
+	var b [32]byte
+
+	der, err := x509.MarshalPKIXPublicKey(cert.PublicKey)
+	if err != nil {
+		return s, errors.New("")
+	}
+
+	b = sha256.Sum256(der)
+	s = base64.StdEncoding.EncodeToString(b[:])
+
+	return s, nil
+}
+
+func violation (r report, ip string, user_agent string, kpins []KnownPin, certs_s_chain []Certificate, certs_v_chain []Certificate) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	report_id, err := db_report(tx, r, ip, user_agent)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	err = pins(tx, report_id, kpins)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	err = certs_s(tx, report_id, certs_s_chain)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	err = certs_v(tx, report_id, certs_v_chain)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func db_report(tx *sql.Tx, r report, request_ip string, user_agent string) (int64, error) {
+	var report_id int64
+
+	date_time := r.DateTime.Format("2006-01-02 15:04:05")
+	effective_expiration_date := r.EffectiveExpirationDate.Format("2006-01-02 15:04:05")
+	include_subdomains := 0
+	if r.IncludeSubdomains == true {
+		include_subdomains = 1
+	}
+
+	res, err := tx.Exec("INSERT INTO reports (created_at, request_ip, user_agent, date_time, effective_expiration_date, hostname, noted_hostname, port, include_subdomains) VALUES (UTC_TIMESTAMP(), ?, ?, ?, ?, ?, ?, ?, ?)", request_ip, user_agent, date_time, effective_expiration_date, r.Hostname, r.NotedHostname, r.Port, include_subdomains)
+	if err != nil {
+		return report_id, errors.New("1")
+	}
+	report_id, err = res.LastInsertId()
+	if err != nil {
+		return report_id, errors.New("2")
+	}
+
+	return report_id, nil
+}
+
+func pins (tx *sql.Tx, report_id int64, pins []KnownPin) error {
+	var pin_id int
+
+	for _, pin := range pins {
+		_, err := tx.Exec("INSERT INTO pins (name, value) VALUES (?, ?)", pin.Name, pin.Value)
+		if err != nil {
+			if mysqlError, ok := err.(*mysql.MySQLError); ok {
+				if mysqlError.Number == 1062 {
+					err = nil
+				}
+			}
+		}
+		if err != nil {
+			return err
+		}
+
+		err = tx.QueryRow("SELECT id FROM pins WHERE name = ? AND value = ?", pin.Name, pin.Value).Scan(&pin_id)
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.Exec("INSERT INTO report_pins (report_id, pin_id) VALUES (?, ?)", report_id, pin_id)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func certs_s (tx *sql.Tx, report_id int64, certs []Certificate) error {
+	return db_certs("s", tx, report_id, certs)
+}
+func certs_v (tx *sql.Tx, report_id int64, certs []Certificate) error {
+	return db_certs("v", tx, report_id, certs)
+}
+
+func db_certs (t string, tx *sql.Tx, report_id int64, certs []Certificate) error {
+	var cert_id int
+
+	for _, cert := range certs {
+		_, err := tx.Exec("INSERT INTO certs (cert, pin) VALUES (?, ?)", cert.PEM, cert.Pin)
+		if err != nil {
+			if mysqlError, ok := err.(*mysql.MySQLError); ok {
+				if mysqlError.Number == 1062 {
+					err = nil
+				}
+			}
+		}
+		if err != nil {
+			return err
+		}
+
+		err = tx.QueryRow("SELECT id FROM certs WHERE pin = ?", cert.Pin).Scan(&cert_id)
+		if err != nil {
+			return err
+		}
+
+		var query string
+		if t == "s" {
+			query = "INSERT INTO report_s_chain (report_id, cert_id, position) VALUES (?,?,?)"
+		} else {
+			query = "INSERT INTO report_v_chain (report_id, cert_id, position) VALUES (?,?,?)"
+		}
+
+		_, err = tx.Exec(query, report_id, cert_id, cert.Position)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ schema.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE hpkp;
+
+USE hpkp;
+
+CREATE TABLE reports
+(
+	id int unsigned not null auto_increment,
+	created_at datetime not null,
+	request_ip varchar(255) not null,
+	user_agent varchar(255) not null,
+	date_time datetime not null,
+	effective_expiration_date datetime not null,
+	hostname varchar(255) not null,
+	noted_hostname varchar(255) not null,
+	port smallint unsigned not null,
+	include_subdomains tinyint unsigned not null,
+
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE pins
+(
+	id int unsigned not null auto_increment,
+	name CHAR(10) NOT NULL,
+	value CHAR(44) NOT NULL,
+
+	primary key (id)
+);
+
+CREATE TABLE report_pins
+(
+	report_id int unsigned not null,
+	pin_id int unsigned not null,
+
+	primary key (report_id, pin_id)
+);
+
+CREATE TABLE certs
+(
+	id int unsigned not null auto_increment,
+	cert text not null,
+	pin char(44) not null,
+	primary key (id),
+	unique key (pin)
+);
+
+CREATE TABLE report_s_chain
+(
+	report_id int unsigned not null,
+	cert_id int unsigned not null,
+	position tinyint unsigned not null,
+
+	primary key (report_id, cert_id)
+);
+
+CREATE TABLE report_v_chain
+(
+	report_id int unsigned not null,
+	cert_id int unsigned not null,
+	position tinyint unsigned not null,
+
+	primary key (report_id, cert_id)
+);
