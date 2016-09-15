936a3d94d5c696ed27ff57bccc4f125d582b5b36
Jean Paul Galea <jeanpaul@yubico.com>
2016-09-15 15:13:34 +0200

    Schema improvements.
    - foreign key contraints.
    
    - column type adjustments.
    
    - cosmetic changes.

--- schema.sql
+++ schema.sql
@@ -4,16 +4,18 @@ USE hpkp;
 
 CREATE TABLE reports
 (
-	id int unsigned not null auto_increment,
-	created_at datetime not null,
-	request_ip varchar(255) not null,
-	user_agent varchar(255) not null,
-	date_time datetime not null,
-	effective_expiration_date datetime not null,
-	hostname varchar(255) not null,
-	noted_hostname varchar(255) not null,
-	port smallint unsigned not null,
-	include_subdomains tinyint unsigned not null,
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+
+	created_at DATETIME NOT NULL,
+	request_ip VARCHAR(45) NOT NULL,
+	user_agent VARCHAR(255) NOT NULL,
+
+	date_time DATETIME NOT NULL,
+	effective_expiration_date DATETIME NOT NULL,
+	hostname VARCHAR(255) NOT NULL, -- ?
+	noted_hostname VARCHAR(255) NOT NULL, -- ?
+	port SMALLINT UNSIGNED NOT NULL,
+	include_subdomains TINYINT(1) NOT NULL,
 
 	PRIMARY KEY (id)
 );
@@ -29,35 +31,68 @@ CREATE TABLE pins
 
 CREATE TABLE report_pins
 (
-	report_id int unsigned not null,
-	pin_id int unsigned not null,
+	report_id INT UNSIGNED NOT NULL,
+	pin_id INT UNSIGNED NOT NULL,
 
-	primary key (report_id, pin_id)
+	-- doesn't make sense to have duplicate pins,
+	--	even though the rfc allows it.
+	PRIMARY KEY (report_id, pin_id),
+
+	FOREIGN KEY (report_id)
+		REFERENCES report(id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	FOREIGN KEY (pin_id)
+		REFERENCES pins(id)
+		ON UPDATE RESTRICT
+		ON DELETE RESTRICT
 );
 
 CREATE TABLE certs
 (
-	id int unsigned not null auto_increment,
-	cert text not null,
-	pin char(44) not null,
-	primary key (id),
-	unique key (pin)
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	cert TEXT NOT NULL,
+	pin CHAR(44) NOT NULL,
+
+	PRIMARY KEY (id),
+	UNIQUE KEY (pin)
 );
 
 CREATE TABLE report_s_chain
 (
-	report_id int unsigned not null,
-	cert_id int unsigned not null,
-	position tinyint unsigned not null,
+	report_id INT UNSIGNED NOT NULL,
+	cert_id INT UNSIGNED NOT NULL,
 
-	primary key (report_id, cert_id)
+	-- I have yet to see a chain with >255 certs
+	position TINYINT UNSIGNED NOT NULL,
+
+	PRIMARY KEY (report_id, cert_id),
+
+	FOREIGN KEY (report_id)
+		REFERENCES report(id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	FOREIGN KEY (cert_id)
+		REFERENCES certs(id)
+		ON UPDATE RESTRICT
+		ON DELETE RESTRICT
 );
 
 CREATE TABLE report_v_chain
 (
-	report_id int unsigned not null,
-	cert_id int unsigned not null,
-	position tinyint unsigned not null,
+	report_id INT UNSIGNED NOT NULL,
+	cert_id INT UNSIGNED NOT NULL,
 
-	primary key (report_id, cert_id)
+	position TINYINT UNSIGNED NOT NULL,
+
+	PRIMARY KEY (report_id, cert_id)
+
+	FOREIGN KEY (report_id)
+		REFERENCES report(id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	FOREIGN KEY (cert_id)
+		REFERENCES certs(id)
+		ON UPDATE RESTRICT
+		ON DELETE RESTRICT
 );
